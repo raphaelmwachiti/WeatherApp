706ca6fee5e4c949d0e3dd4d9d63c22abd48b77c
raphaelmwachiti <raphaelmwachiti@gmail.com>
2023-07-25 20:04:18 -0700

    commit

--- App.js
+++ App.js
@@ -1,23 +1,13 @@
 import React from 'react';
-import { View, StyleSheet, Text} from 'react-native';
-import { StatusBar } from 'expo-status-bar';
 import { Provider } from 'react-redux';
+
 import store from './src/redux/store';
+import WeatherScreen from './src/screens/WeatherScreen'; 
 
 export default function App() {
   return (
     <Provider store={store}>
-      <View style = {styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <StatusBar style="auto" />
-      </View>
+      <WeatherScreen />
     </Provider>
   );
 }  
-
-const styles = {
-  container: {
-    marginTop: 50,
-  }
-}
-
