2a0d6e6a17b091d6a501d6053bd89912e66d202b
raphaelmwachiti <raphaelmwachiti@gmail.com>
2023-07-25 19:54:28 -0700

    commit

--- App.js
+++ App.js
@@ -1,15 +1,23 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, StyleSheet, Text} from 'react-native';
+import { StatusBar } from 'expo-status-bar';
 import { Provider } from 'react-redux';
 import store from './src/redux/store';
 
 export default function App() {
   return (
     <Provider store={store}>
-      <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <View style = {styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
     </Provider>
   );
+}  
+
+const styles = {
+  container: {
+    marginTop: 50,
+  }
 }
+
